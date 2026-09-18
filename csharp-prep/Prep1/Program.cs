56ba7c62b151a4b7b9c8520a6dacd4905d0af91a
GiovanniMistura <giovizoro@gmail.com>
2026-09-18 13:54:47 -0600

    Programming Parts 1-2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //take the name
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        //take the last name
+        Console.Write("What is your first last name? ");
+        string lastName = Console.ReadLine();
+
+        //print the name and last name with line spacing
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //get the user grade
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        //print the correct letter grade
+        string letter = ""; 
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";   
+        }
+
+        //recognise if there is + or - to the letter grade
+        string plusOrMinor = "";
+        int rest = grade % 10;
+
+        if (rest >= 7 && letter != "A")
+        {
+            plusOrMinor = "+";
+        }
+        else if (rest < 3 && letter != "F")
+        {
+            plusOrMinor = "-";
+        }
+
+
+        Console.WriteLine($"Your letter grade is {letter}{plusOrMinor}");
+
+        //check and print if the user passes the class
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the exam");
+        } 
+        else
+        {
+            Console.WriteLine("You did NOT pass the exam, try again!");
+        }
     }
 }
